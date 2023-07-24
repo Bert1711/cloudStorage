ba76d45548fed4f790e83ae183ffd9d3c3ae8b10
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-07-24 13:58:23 +0300

    Initial commit

---
+++ src/main/java/ru/zaroyan/draftcloudstorage/DraftCloudStorageApplication.java
@@ -0,0 +1,13 @@
+package ru.zaroyan.draftcloudstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftCloudStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DraftCloudStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zaroyan/draftcloudstorage/DraftCloudStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zaroyan.draftcloudstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DraftCloudStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
