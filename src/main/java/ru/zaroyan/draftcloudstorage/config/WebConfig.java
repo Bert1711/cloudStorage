b7d7b13c9aae27eb6c7107427c4d701f5f9dac92
Albert Zaroyan <albertzaroyan@yandex.ru>
2023-08-24 21:35:40 +0300

    "add WebConfig"

---
+++ src/main/java/ru/zaroyan/draftcloudstorage/config/WebConfig.java
@@ -0,0 +1,23 @@
+package ru.zaroyan.draftcloudstorage.config;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Zaroyan
+ */
+
+
+@Configuration
+@EnableWebMvc
+class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:8081")
+                .allowedMethods("*");
+    }
+}
